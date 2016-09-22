d19c39942711badc14b1c0a7f4bda28557631601
Christian Diener <ch.diener@gmail.com>
2016-09-22 15:23:11 -0500

    Added human seq removal

--- mbtools/R/remove_human.r
+++ mbtools/R/remove_human.r
@@ -2,6 +2,9 @@
 #
 # Apache license 2.0. See LICENSE for more information.
 
+HS_GENOME <- "ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/
+dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
+
 #' Builds index files for human sequence identification
 #'
 #' This function has to be run only once to enable analysis.
@@ -14,6 +17,23 @@
 #'
 #' @export
 build_index <- function(where="./bmtagger", genome_file=NA) {
+    if (is.na(genome_file)) {
+        loc <- file.path(where, "hg38.fa.gz")
+        cat("No genome file given, downloading hg38...\n")
+        download.file(HS_GENOME, loc)
+        genome_file <- loc
+    }
+
+    cat("Building reference bitmask...\n")
+    system2("bmtool", c("-d", genome_file, "-o",
+                        file.path(where, "ref.bitmask"), "-w", "18"))
+
+    cat("Building srprism index...\n")
+    system2("srprism", c("mkindex", "-i", genome_file, "-o",
+                         file.path(where, "ref.srprism"), "-M", "7168"))
+
+    cat("Building blast database...\n")
+    system2("makeblastdb", c("-in", genome_file, "-dbtype", "nucl"))
 
 }
 
@@ -24,14 +44,56 @@ build_index <- function(where="./bmtagger", genome_file=NA) {
 #'
 #' @param reads A character vector containing the read files in fastq format.
 #' @param out A folder to which to save the filtered fastq files.
-#' @param barcode_files Additional barcode file that should be filtered as well.
-#' @param index Where to find the previously generated index files.
+#' @param index Additional barcode file that should be filtered as well.
+#' @param where Where to find the previously generated index files.
 #' @return A numeric vector with two entries. The number of sequences after
-#'  filtering (non-human), and the number of removed sequnces (human).
+#'  filtering (non-human), and the number of removed sequences (human).
 #' @examples
 #'  NULL
 #'
 #' @export
-remove_human <- function(reads, out, barcode_files=NA, index="./bmtagger") {
+remove_human <- function(reads, index=NA, out, where="./bmtagger") {
+    paired <- length(reads) == 2 & !any(is.na(reads))
 
+    cat("Finding human sequences...")
+    if (!paired) {
+        system2("bmtagger.sh", c("-b", file.path(where, "ref.bitmask"), "-x",
+                                 file.path(where, "ref.srprism"), "-T",
+                                 file.path(where, "tmp"), "-q", "1", "-1", "-o",
+                                 file.path(where, "human.txt")))
+    } else {
+        system2("bmtagger.sh", c("-b", file.path(where, "ref.bitmask"), "-x",
+                                 file.path(where, "ref.srprism"), "-T",
+                                 file.path(where, "tmp"), "-q", "1", "-1",
+                                 reads[1], "-2", reads[2], "-o",
+                                 file.path(where, "human.txt")))
+    }
+
+
+    human_ids <- read.table(file.path(where, "human.txt"))[, 1]
+    new_files <- file.path(out, basename(reads))
+
+    streams <- list(f = FastqStreamer(reads[1]))
+    if (!is.na(index)) {
+        streams$i <- FastqStreamer(index)
+    }
+    if (paired) {
+        streams$r <- FastqStreamer(reads[2])
+    }
+
+    counts <- vapply(1:length(streams), function(i) {
+        n <- 0
+        repeat {
+            reads <- yield(streams[[i]])
+            if (length(reads) == 0) break
+            rem <- !(id(reads) %in% human_ids)
+            writeFastq(reads[rem], new_files[i])
+            n <- n + length(reads[rem])
+        }
+        n
+    },
+    0)
+    lapply(streams, close)
+
+    return(c(reads = counts[1], removed = length(human_ids)))
 }
--- mbtools/tests/testthat/test_bmtagger.R
+++ mbtools/tests/testthat/test_bmtagger.R
@@ -1,5 +1,38 @@
 context("H. sapiens sequence removal")
 
-test_that("bmtagger works", {
-    expect_true(TRUE)
+test_that("bmtagger index files can be created", {
+    with_mock(
+        download.file = function(...) print("download"),
+        system2 = function(...) print("system2"),
+        expect_output(build_index(), "download"),
+        expect_output(build_index(genome_file = "mock.fa"), "system2")
+    )
+})
+
+test_that("sequences can be removed", {
+    d <- tempdir()
+    seqs <- replicate(100,
+        paste(sample(c("A", "C", "G", "T"), 100, replace = TRUE),
+                     collapse = ""))
+    quals <- replicate(100, paste(rep("@", 100), collapse = ""))
+    sr <- ShortReadQ(sread = DNAStringSet(seqs),
+                     quality = BStringSet(quals),
+                     id = BStringSet(1:100))
+    old_files <- list.files(d, "fastq", recursive = TRUE, full.names = TRUE)
+    file.remove(old_files)
+    writeFastq(sr, file.path(d, "f.fastq"))
+    writeFastq(sr, file.path(d, "r.fastq"))
+    writeFastq(sr, file.path(d, "i.fastq"))
+
+    reads <- file.path(d, c("f.fastq", "r.fastq"))
+    index <- file.path(d, "i.fastq")
+    write("1", file = file.path(d, "human.txt"))
+    dir.create(file.path(d, "nh"), showWarnings = FALSE)
+
+    with_mock(system2 = function(...) print("system2"),
+        out <- capture.output(counts <- remove_human(reads, index,
+            file.path(d, "nh"), d)),
+        expect_true(grepl("system2", out)),
+        expect_equal(counts, c(reads = 99, removed = 1))
+    )
 })
