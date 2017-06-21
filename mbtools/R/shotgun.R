751f8db1a86783d039d77c46a7321098feb10f86
Christian Diener <ch.diener@gmail.com>
2017-06-21 12:48:15 -0500

    Added slimm shotgun functions

---
+++ mbtools/R/shotgun.R
@@ -0,0 +1,124 @@
+# Copyright 2016 Christian Diener <mail[at]cdiener.com>
+#
+# Apache license 2.0. See LICENSE for more information.
+
+#' Aligns metagenomic shotgun sample against a reference
+#'
+#' This method uses bowtie2 and should not be too sensitive to the used pre-
+#' processing.
+#'
+#' @param reads A data frame containing the paths to the forward and reverse 
+#'  reads in fastq format. Needs to have at least columns "id" and "forward".
+#'  If the samples are paired you also need a column "reverse".
+#' @param index_basename Path and basename of the index against which to align.
+#' @param threads How many threads to use for bowtie2.
+#' @param alignment_folder A folder to which to save log output and the 
+#'  generated alignment.
+#' @param bam Whether to output alignments in BAM format (requires samtools).
+#' @param bowtie2_path Path to the bowtie executables.
+#' @param samtools_path Path to samtools.
+#' @return A data frame mapping the alignments to the sample IDs. It will 
+#'  contain the following columns:
+#'  \itemize{
+#'  \item{id}{the id of the sample}
+#'  \item{success}{whether alignment executed without error}
+#'  \item{log}{the log output for the alignment}
+#'  \item{alignment}{the file containing the alignment}
+#'  }
+#' @examples
+#'  NULL
+#'
+#' @export
+align_bowtie2 <- function(reads, index_basename, threads=1,
+                          alignment_folder = "alignments", bam = TRUE,
+                          bowtie2_path = NULL, samtools_path = NULL) {
+    env <- character()
+    if (!is.null(bowtie2_path)) {
+        env <- paste0("PATH=", bowtie2_path)
+    }
+    if (!is.null(samtools_path)) {
+        env <- paste0(env, ":", samtools_path)
+    }
+    paired <- "reverse" %in% names(reads)
+
+    write("Aligning reads to microbial genomes...", file="")
+    alignments <- pbapply(reads, 1, function(read) {
+        read <- as.list(read)
+        args <- c("-x", index_basename)
+        if (paired) {
+            args <- append(args, c("-1", read$forward, "-2", read$reverse))
+        } else {
+            args <- append(args, c("-U", read$forward))
+        }
+        args <- append(args, c("-q", "--no-unal", "--mm", "-p", threads, "-k",
+                               "60"))
+
+        log_file <- file.path(alignment_folder, paste0(read$id, ".log"))
+        if (bam) {
+            out_path <- file.path(alignment_folder, paste0(read$id, ".bam"))
+            args <- append(args, c(paste0("2>", log_file), "|", "samtools",
+                           "view", "-bS", "-", ">", out_path))
+        } else {
+            out_path <- file.path(alignment_folder, paste0(read$id, ".sam"))
+            args <- append(args, c("-S", out_path, "2>", log_file))
+        }
+        success <- system2("bowtie2", args = args, env = env)
+
+        return(data.frame(id = read$id, success = success == 0, log = log_file,
+                          alignment = out_path))
+    })
+
+    return(do.call(rbind.data.frame, alignments))
+}
+
+
+#' Quantifies abundances for the bacteria using SLIMM
+#'
+#' @param alignments A data frame as output by \code(\link(align_bowtie2))
+#' @param slimm_db Path for the SLIMM data base.
+#' @return A data table counting reads and relative abundances for all found
+#'  bacteria for several taxonomic ranks. It will contain the following columns:
+#'  \itemize{
+#'  \item{id}{id of the sample}
+#'  \item{rank}{the name of the taxonomic rank, for instance "genus"}
+#'  \item{name}{the value of the rank, for instance "Escherichia"}
+#'  \item{reads}{the number of reads in that rank}
+#'  \item{relative}{the relative abundance of the rank in [0,1]}
+#'  \item{coverage}{the coverage of the rank}
+#'  }
+#' @examples
+#'  NULL
+#'
+#' @export
+#' @importFrom data.table fread rbindlist
+slimm <- function(alignments, slimm_db) {
+    if (any(alignments$success != 0)) {
+        stop("some alignments were not successful!")
+    }
+    reports <- tempdir()
+    write("Running SLIMM...", file="")
+    ecodes <- pbapply(alignments$alignment, function(al) {
+        ecode <- system2("slimm", args=c("-m", slimm_db, "-o", reports, al),
+                         stdout = NULL, stderr=file.path(reports, "slimm.log"))
+        return(ecode)
+    })
+
+    if (any(ecodes != 0)) {
+        stop(paste0("slimm terminated with an error, logs can be found in",
+                    file.path(reports, "slimm.log")))
+    }
+
+    write("Summarizing results", file="")
+    tsvs <- list.files(reports, pattern="_reported.tsv")
+    dts <- pbapply(tsvs, function(file) {
+        elements <- strsplit(basename(file), "_")[[1]]
+        id <- elements[1]
+        rank <- elements[2]
+        dt <- fread(file, drop=1, col.names=c("name", "taxid", "reads", 
+                    "relative", "contributors", "coverage"))
+        dt$rank <- rank
+        dt$id <- id
+    })
+
+    return(rbindlist(dts)) 
+}
